50389eded2e42589d9fc8ab093101700658a0ddc
Ray Caddick <rrcaddick@gmail.com>
2025-02-21 12:16:47 +0200

    fix: resolved pre-commit issues

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,6 +12,7 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
+    default="gU3KMuQp3aR72SJWhVdpoqXMRq7EWJm5bMq70fktSmwEU2ZRn5OK3nDSJPu7JDLG",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
--- farmyard_manager/static/js/project.js
+++ farmyard_manager/static/js/project.js
@@ -1,3 +1,3 @@
-import '../sass/project.scss';
+// import "../sass/project.scss";
 
 /* Project specific Javascript goes here. */
